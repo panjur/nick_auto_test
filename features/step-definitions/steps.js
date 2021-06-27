37d82b8f6be3efd85c9ec1ae8e7093b1b2c055e0
RahulR <pandya.rahul00@gmail.com>
2021-06-27 19:14:38 +0530

    📦 Sign up flow added

--- features/step-definitions/steps.js
+++ features/step-definitions/steps.js
@@ -1,23 +1,321 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
 
 const LoginPage = require('../pageobjects/login.page');
+const SignUpPage = require('../pageobjects/signup.page');
 
 const pages = {
-    login: LoginPage
-}
+  login: LoginPage,
+  signup: SignUpPage,
+};
 
 Given(/^I am on the (\w+) page$/, (page) => {
-    pages[page].open();
+  pages[page].open();
 });
 
 When(/^I login with (.+) and (.+)$/, (username, password) => {
-    LoginPage.login(username, password)
+  LoginPage.login(username, password);
 });
 
 Then(/^I should text (.*)$/, (message) => {
-    expect(browser.$("body")).toHaveTextContaining(message);
+  expect(browser.$('body')).toHaveTextContaining(message);
 });
 
 Then(/^I reload browser session/, () => {
-    browser.reloadSession();
+  browser.reloadSession();
+});
+
+When(/^I select (.+) and (.+)$/, (gender, lookingFor) => {
+  SignUpPage.selectGender(gender);
+  SignUpPage.selectLookingFor(lookingFor);
+});
+
+When(/^I submit with (.+)$/, (email) => {
+  SignUpPage.enterEmail(email);
+  SignUpPage.submit();
+});
+
+When(/^I enter (.+) as name$/, (name) => {
+  SignUpPage.enterName(name);
+});
+
+When(/^I click on Next button$/, () => {
+  SignUpPage.clickNext();
+  SignUpPage.wait(2);
+});
+
+When(/^I select (.+) as gender$/, (gender) => {
+  SignUpPage.wait(2);
+  SignUpPage.selectMyGender(gender);
+});
+
+When(/^I enter (.+) as email$/, (email) => {
+  SignUpPage.enterEmailAddress(email);
+});
+
+When(/^I enter (.+) as password$/, (password) => {
+  SignUpPage.enterPassword(password);
+});
+
+When(/^I select (.+) for Birthday$/, (date) => {
+  SignUpPage.selectDate(date);
+});
+
+//DEBUG
+When(/^Wait for next one$/, () => {
+  console.log("I'll wait");
+  browser.pause(30000);
+});
+
+When(/^I select (.+) as Country$/, (country) => {
+  SignUpPage.selectCountry(country);
+});
+
+When(/^I select (.+) as City$/, (city) => {
+  SignUpPage.selectCity(city);
+});
+
+When(/^I select (.+) as District$/, (district) => {
+  SignUpPage.selectDistrict(district);
+});
+
+When(/^I select (.+) for the how far member location$/, (location) => {
+  SignUpPage.selectProximateArea(location);
+  SignUpPage.wait(2);
+});
+
+When(/^I select (.+) for monthly income$/, (income) => {
+  SignUpPage.selectIncome(income);
+  SignUpPage.wait(2);
+});
+
+When(/^I select keep it secret as (.+)$/, (isSecret) => {
+  SignUpPage.keepIncomeSecret(isSecret);
+});
+
+When(
+  /^I select (.+) for the finance situation of member$/,
+  (isFinanceImportant) => {
+    SignUpPage.importanceOfMemberIncome(isFinanceImportant);
+    SignUpPage.wait(2);
+  },
+);
+
+When(/^I select Your Merital Status as (.+)$/, (status) => {
+  SignUpPage.selectMaritalStatus(status);
+});
+
+When(/^I select (.+) in Children$/, (hasChildren) => {
+  SignUpPage.selectChildrenStatus(hasChildren);
+  SignUpPage.wait(2);
+});
+
+When(/^I add the details as (.+)$/, (comment) => {
+  SignUpPage.enterDetails(comment);
+});
+
+When(/^I select (.+) in occupation$/, (occupation) => {
+  SignUpPage.selectOccupation(occupation);
+});
+
+When(/^I have (.+) Children$/, (children) => {
+  if (children !== '' && children !== '0') {
+    SignUpPage.selectChildren(children);
+  }
+});
+
+When(/^I select (.+) for is children living with you$/, (isWithYou) => {
+  SignUpPage.selectChildrenLiving(isWithYou);
+  SignUpPage.wait(2);
+});
+
+When(/^I select (.+) for the accept member who has child$/, (hasChildren) => {
+  SignUpPage.acceptMemberWithChild(hasChildren);
+  SignUpPage.wait(2);
+});
+
+When(
+  /^I select (.+) for the accept member live with child$/,
+  (liveWithChildren) => {
+    SignUpPage.acceptMemberLivingWithChild(liveWithChildren);
+    SignUpPage.wait(2);
+  },
+);
+
+When(/^I select (.+) for if I want children$/, (wantChildren) => {
+  SignUpPage.ifWantChildren(wantChildren);
+  SignUpPage.wait(2);
+});
+
+When(/^I select (.+) for who can see my profile$/, (isPrivacy) => {
+  SignUpPage.isProfilePrivate(isPrivacy);
+  SignUpPage.wait(2);
+});
+
+When(/^I (.+) for the accepting the agreement$/, (isAgreement) => {
+  SignUpPage.acceptAgreement(isAgreement);
+  SignUpPage.wait(2);
+});
+
+When(
+  /^I (.+) for the receiving the promotional mails$/,
+  (isReceivePromoMail) => {
+    SignUpPage.receivePrmo(isReceivePromoMail);
+  },
+);
+
+When(/^I select my height as (.+)$/, (height) => {
+  SignUpPage.selectHeight(height);
+});
+
+When(/^I select my weight as (.+)$/, (weight) => {
+  SignUpPage.selectWeight(weight);
+});
+
+When(/^I select my eye color as (.+)$/, (eyeColor) => {
+  SignUpPage.selecteyeColor(eyeColor);
+  SignUpPage.wait(2);
+});
+
+When(/^I select my hair color as (.+)$/, (hairColor) => {
+  SignUpPage.selectHairColor(hairColor);
+  SignUpPage.wait(2);
+});
+
+When(/^I select I am physically (.+)$/, (physique) => {
+  SignUpPage.selectPhysique(physique);
+  SignUpPage.wait(2);
+});
+
+When(/^I select (.+) importance of member weight$/, (weight) => {
+  SignUpPage.selectMemberWeightImportance(weight);
+  SignUpPage.wait(2);
+});
+
+When(/^I select (.+) importance of member size$/, (size) => {
+  SignUpPage.selectMemberSizeImportance(size);
+  SignUpPage.wait(2);
+});
+
+When(/^I select (.+) importance of member appearance$/, (appearance) => {
+  SignUpPage.selectMemberAppearanceImportance(appearance);
+  SignUpPage.wait(2);
+});
+
+When(/^I select (.+) as my dressing style$/, (dressing) => {
+  SignUpPage.selectDressingStyle(dressing);
+  SignUpPage.wait(2);
+});
+
+When(/^I select (.+) importance of member dressing style$/, (dressing) => {
+  SignUpPage.selectMemberDressingImportance(dressing);
+  SignUpPage.wait(2);
+});
+
+When(/^I select my degree as (.+)$/, (degree) => {
+  SignUpPage.selectDegree(degree);
+  SignUpPage.wait(2);
+});
+
+When(/^I enter my graduate school (.+)$/, (school) => {
+  SignUpPage.enterSchoolName(school);
+  SignUpPage.wait(2);
+});
+
+When(/^I select (.+) for my current is_schooling$/, (isSchooling) => {
+  SignUpPage.selectIsSchooling(isSchooling);
+  SignUpPage.wait(2);
+});
+
+When(/^I select (.+) as my current study$/, (currentStudy) => {
+  SignUpPage.selectSchoolingFor(currentStudy);
+  SignUpPage.wait(2);
+});
+
+When(/^I select (.+) importance of member degree$/, (degree) => {
+  SignUpPage.selectMemberDegreeImportance(degree);
+  SignUpPage.wait(2);
+});
+
+When(/^I select (.+) for member degree preference$/, (preference) => {
+  SignUpPage.selectMemberPreferredDegreeImportance(preference);
+  SignUpPage.wait(2);
+});
+
+When(/^I select (.+) for my smoking$/, (isSmoke) => {
+  SignUpPage.selectIsSmoking(isSmoke);
+  SignUpPage.wait(2);
+});
+
+When(/^I select (.+) importance of member smoking$/, (isSmoke) => {
+  SignUpPage.selectIsMemberSmoking(isSmoke);
+  SignUpPage.wait(2);
+});
+
+When(/^I select (.+) as my alcohol consumption$/, (isDrink) => {
+  SignUpPage.selectIsDrinking(isDrink);
+  SignUpPage.wait(2);
+});
+
+When(/^I select (.+) importance of member drinking$/, (isDrink) => {
+  SignUpPage.selectIsMemberDrinking(isDrink);
+  SignUpPage.wait(2);
+});
+
+When(/^I select with whom I live with (.+)$/, (liveWith) => {
+  SignUpPage.selectILiveWith(liveWith);
+  SignUpPage.wait(2);
+});
+
+When(/^I select (.+) for if I have my house$/, (hasHouse) => {
+  SignUpPage.selectHasOwnHouse(hasHouse);
+  SignUpPage.wait(2);
+});
+
+When(/^I select I work as (.+)$/, (workType) => {
+  SignUpPage.selectWorkType(workType);
+  SignUpPage.wait(2);
+});
+
+When(/^I select my work position as (.+)$/, (position) => {
+  SignUpPage.selectWorkPosition(position);
+  SignUpPage.wait(2);
+});
+
+When(/^I select I know (.+) as well as other language$/, (language) => {
+  SignUpPage.selectLanguge(language);
+  SignUpPage.wait(2);
+});
+
+When(/^I select (.+) as Other thing I like$/, (aboutMe) => {
+  SignUpPage.selectAboutMe(aboutMe);
+  SignUpPage.wait(2);
+});
+
+When(/^I select (.+) as thing that describe me$/, (describeMe) => {
+  SignUpPage.selectDescribeMe(describeMe);
+  SignUpPage.wait(2);
+});
+
+When(/^I select (.+) as my hobbies$/, (hobby) => {
+  SignUpPage.selectHobby(hobby);
+  SignUpPage.wait(2);
+});
+
+When(/^I enter (.+) as my dream person$/, (about) => {
+  SignUpPage.enterAboutDreamPerson(about);
+  SignUpPage.wait(2);
+});
+
+When(/^I enter (.+) as about myself and life$/, (about) => {
+  SignUpPage.enterAboutSelf(about);
+  SignUpPage.wait(2);
+});
+
+When(/^I upload my picture (.+)$/, (photo) => {
+  if (photo.toLowerCase() === 'later') SignUpPage.uploadPictureLater(photo);
+  else SignUpPage.uploadPicture(photo);
+});
+
+Then(/^I succesfully completed the registration$/, () => {
+  expect(browser.$('a img[src*="home-icon"]')).toBeDisplayed();
 });
--- wdio.conf.js
+++ wdio.conf.js
@@ -1,86 +1,91 @@
 exports.config = {
-    runner: 'local',
-    // hostname: process.env.HUB_HOST || 'localhost',
-    // port: 4444,
-    path: '/wd/hub',
-    specs: [
-        './features/**/*.feature'
-    ],
-    exclude: [
-        // 'path/to/excluded/files'
-    ],
-    maxInstances: 10,
-    capabilities: [{
-        maxInstances: 5,
-        //
-        browserName: 'chrome',
-        acceptInsecureCerts: true,
-        "goog:chromeOptions": {
-            "excludeSwitches": [ "enable-automation" ],
-            "mobileEmulation": { deviceName: 'iPhone X' },
-            "args":["disable-blink-features=AutomationControlled"]
-         }
-    }],
-    logLevel: 'warn',
-    //
-    // Set specific log levels per logger
-    // loggers:
-    // - webdriver, webdriverio
-    // - @wdio/applitools-service, @wdio/browserstack-service, @wdio/devtools-service, @wdio/sauce-service
-    // - @wdio/mocha-framework, @wdio/jasmine-framework
-    // - @wdio/local-runner
-    // - @wdio/sumologic-reporter
-    // - @wdio/cli, @wdio/config, @wdio/utils
-    // Level of logging verbosity: trace | debug | info | warn | error | silent
-    // logLevels: {
-    //     webdriver: 'info',
-    //     '@wdio/applitools-service': 'info'
-    // },
-    //
-    // If you only want to run your tests until a specific amount of tests have failed use
-    // bail (default is 0 - don't bail, run all tests).
-    bail: 0,
-    waitforTimeout: 5000,
-    connectionRetryTimeout: 120000,
-    connectionRetryCount: 3,
-    framework: 'cucumber',
-    reporters: ['spec'],
-    cucumberOpts: {
-        // <string[]> (file/dir) require files before executing features
-        require: ['./features/step-definitions/steps.js'],
-        // <boolean> show full backtrace for errors
-        backtrace: false,
-        // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
-        requireModule: [],
-        // <boolean> invoke formatters without executing steps
-        dryRun: false,
-        // <boolean> abort the run on first failure
-        failFast: false,
-        // <string[]> (type[:path]) specify the output format, optionally supply PATH to redirect formatter output (repeatable)
-        format: ['pretty'],
-        // <boolean> hide step definition snippets for pending steps
-        snippets: true,
-        // <boolean> hide source uris
-        source: true,
-        // <string[]> (name) specify the profile to use
-        profile: [],
-        // <boolean> fail if there are any undefined or pending steps
-        strict: false,
-        // <string> (expression) only execute the features or scenarios with tags matching the expression
-        tagExpression: '',
-        // <number> timeout for step definitions
-        timeout: 60000,
-        // <boolean> Enable this config to treat undefined definitions as warnings.
-        ignoreUndefinedDefinitions: false
+  runner: 'local',
+  // hostname: process.env.HUB_HOST || 'localhost',
+  // port: 4444,
+  path: '/wd/hub',
+  specs: ['./features/**/*.feature'],
+  exclude: [
+    // 'path/to/excluded/files'
+  ],
+  maxInstances: 10,
+  capabilities: [
+    {
+      maxInstances: 5,
+      //
+      browserName: 'chrome',
+      acceptInsecureCerts: true,
+      'goog:chromeOptions': {
+        excludeSwitches: ['enable-automation'],
+        mobileEmulation: { deviceName: 'iPhone X' },
+        args: ['disable-blink-features=AutomationControlled'],
+      },
     },
-    afterStep: function (step, scenario, result) {
-        const path = require('path');
-        if (result.passed) {
-            return;
-        }
-        const screenshotFileName = encodeURIComponent(`${scenario.name.replace(/\s+/g, "-")}`).replace(/%../, ".");
-        const filePath = path.resolve(path.join(process.cwd(), "screenshots"), `${screenshotFileName}.png`);
+  ],
+  logLevel: 'warn',
+  //
+  // Set specific log levels per logger
+  // loggers:
+  // - webdriver, webdriverio
+  // - @wdio/applitools-service, @wdio/browserstack-service, @wdio/devtools-service, @wdio/sauce-service
+  // - @wdio/mocha-framework, @wdio/jasmine-framework
+  // - @wdio/local-runner
+  // - @wdio/sumologic-reporter
+  // - @wdio/cli, @wdio/config, @wdio/utils
+  // Level of logging verbosity: trace | debug | info | warn | error | silent
+  // logLevels: {
+  //     webdriver: 'info',
+  //     '@wdio/applitools-service': 'info'
+  // },
+  //
+  // If you only want to run your tests until a specific amount of tests have failed use
+  // bail (default is 0 - don't bail, run all tests).
+  bail: 0,
+  waitforTimeout: 5000,
+  connectionRetryTimeout: 120000,
+  connectionRetryCount: 3,
+  framework: 'cucumber',
+  reporters: ['spec'],
+  cucumberOpts: {
+    // <string[]> (file/dir) require files before executing features
+    require: ['./features/step-definitions/steps.js'],
+    // <boolean> show full backtrace for errors
+    backtrace: false,
+    // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
+    requireModule: [],
+    // <boolean> invoke formatters without executing steps
+    dryRun: false,
+    // <boolean> abort the run on first failure
+    failFast: false,
+    // <string[]> (type[:path]) specify the output format, optionally supply PATH to redirect formatter output (repeatable)
+    format: ['pretty'],
+    // <boolean> hide step definition snippets for pending steps
+    snippets: true,
+    // <boolean> hide source uris
+    source: true,
+    // <string[]> (name) specify the profile to use
+    profile: [],
+    // <boolean> fail if there are any undefined or pending steps
+    strict: false,
+    // <string> (expression) only execute the features or scenarios with tags matching the expression
+    tagExpression: '',
+    // <number> timeout for step definitions
+    timeout: 60000,
+    // <boolean> Enable this config to treat undefined definitions as warnings.
+    ignoreUndefinedDefinitions: false,
+  },
+  afterStep: function (step, scenario, result) {
+    const path = require('path');
+    if (result.passed) {
+      return;
+    }
+    const screenshotFileName = encodeURIComponent(
+      `${scenario.name.replace(/\s+/g, '-')}`,
+    ).replace(/%../, '.');
+    const filePath = path.resolve(
+      path.join(process.cwd(), 'screenshots'),
+      `${screenshotFileName}.png`,
+    );
 
-        browser.saveScreenshot(filePath);
-    },
-}
+    browser.saveScreenshot(filePath);
+  },
+};
